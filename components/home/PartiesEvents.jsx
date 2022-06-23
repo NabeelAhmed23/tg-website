fdf7be7942e82e5b8f8ebf43869fddb3fb30b845
Nabeel Ahmed <NabeelAhmed23>
2022-06-23 18:25:02 +0500

    New small tweaks from QA

--- components/home/PartiesEvents.jsx
+++ components/home/PartiesEvents.jsx
@@ -5,7 +5,7 @@ import GroupTripsComponent from '../common/GroupTripsComponent'
 export class PartiesEvents extends Component {
     render() {
         return (
-            <section className='bg-secondary theme-x-padding py-6 lg:py-8 xl:py-10 2xl:py-14 mt-20 3xl:mt-32 '>
+            <section className='bg-secondary theme-x-padding py-6 lg:py-8 xl:py-10 2xl:py-14 mt-32 2xl:mt-24 '>
                 <div className='grid lg:grid-cols-2 gap-y-6 gap-x-8'>
                     <PartyComponent />
                     <GroupTripsComponent />
