f70adad12aa22d7add4883dbd28987b09b771755
NabeelAhmed23 <nabeel.ahmed@mtp.tech>
2022-07-21 19:04:25 +0500

    new changes

--- pages/Layout.jsx
+++ pages/Layout.jsx
@@ -1,14 +1,17 @@
 import PropTypes from "prop-types";
 import HeaderWrapper from "../components/header/HeaderWrapper";
 import MainFooter from "../components/footer/MainFooter";
-import Head from "next/head"
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <>
-    <Head>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
-    </Head>
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
       <HeaderWrapper />
       <main>{children}</main>
       <MainFooter />
