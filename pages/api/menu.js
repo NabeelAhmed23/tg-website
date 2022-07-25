9694778bd585d1e5dbc8a0da874168f4a9900e2b
NabeelAhmed23 <nabeel.ahmed@mtp.tech>
2022-07-25 13:10:15 +0500

    City page started

--- pages/api/menu.js
+++
@@ -1,8 +0,0 @@
-import { getMegaMenu } from "../../utils/services/ApiCalls";
-
-export default async function handler(req, res) {
-  
-  return new Promise((resolve, reject) => {
-const response = await getMegaMenu();
-  })
-}
